28b1c674ca13129cf67ec576c2870ddf1a20726e
hnt04 <105774543+hnt04@users.noreply.github.com>
2022-07-19 16:03:30 +0700

    first commit

---
+++ src/components/TaskList.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import TaskItem from './TaskItem'
+
+function TaskList({tasks, showIncomplete, setTaskStatus, removeTask, setShowIncomplete}) {
+  return (
+    <div>
+        <ul className="task-list">
+            {tasks
+            .filter((task)=>showIncomplete ? task.status !== 1 : true)
+            .map((task)=> (
+                <TaskItem 
+                key={task.id}
+                task={task} 
+                showIncomplete={showIncomplete} 
+                setTaskStatus={setTaskStatus} 
+                removeTask={removeTask}></TaskItem>
+            ))}
+        </ul>
+        <div className = "filter-wrapper">
+            <label htmlFor="filter" className="filter-label">Show incomplete tasks only</label>
+            <input 
+            type ="checkbox" 
+            id="filter" 
+            checked={showIncomplete}
+            onChange={(e) => setShowIncomplete(e.target.checked)}></input>
+        </div>
+    </div>
+  );
+}
+
+export default TaskList;
